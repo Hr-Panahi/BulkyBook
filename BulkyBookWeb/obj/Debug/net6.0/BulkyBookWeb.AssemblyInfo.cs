ff6a17b155065fcedb3801dde3b67489596a7812
Hamid Reza Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2024-02-23 12:42:22 +0330

    Add: toastr alerts
    Added toastr alerts.
    toastr is a Javascript library for non-blocking notifications.

--- BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
+++ BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1199cdfb5965b9929792a07e2a4d21ebed6b83a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b52d1900c927ef7f5a80ca4e29d9dcfd1cf4c745")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
